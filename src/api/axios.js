03ce7255f3f1ca51b809f0bbceb00a7a94a4428f
unknown <alqsyrzynb07@gmail.com>
2026-09-18 16:39:24 +0300

    product-page

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,35 +1,36 @@
-import axios from "axios";
-import Cookies from "js-cookie";
+
+import axios from "axios"
+import Cookies from "js-cookie"
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "https://e-commerce-api-3wara.vercel.app",
- withCredentials : true,
-});
+  baseURL: "/api"
+})
 
-// Request Interceptor → إضافة التوكن
-api.interceptors.request.use((config) => {
-  const token = Cookies.get("store_token");
+api.interceptors.request.use(config => {
+  const token = Cookies.get("store_token")
+
   if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+    config.headers.Authorization = `Bearer ${token}`
   }
-  return config;
-});
 
-// Response Interceptor → التعامل مع 401
+  return config
+})
+
 api.interceptors.response.use(
-  (response) => response,
-  (error) => {
+  response => response,
+  error => {
     if (error.response?.status === 401) {
-      Cookies.remove("store_token");
-      Cookies.remove("store_user");
+      Cookies.remove("store_token")
+      Cookies.remove("store_user")
 
-      // نروح لصفحة اللوجين بس لو مش فيها أصلاً
       if (!window.location.pathname.startsWith("/login")) {
-        window.location.href = "/login";
+        window.location.href = "/login"
       }
     }
-    return Promise.reject(error);
-  }
-);
 
-export default api;
+    return Promise.reject(error)
+  }
+)
+
+export default api
+
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,9 +1,11 @@
 import { Routes, Route } from "react-router-dom";
+import ProductDetail from "../pages/ProductDetail";
 
 export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<div >ALLINONE STORE</div>} />
+          <Route path="/products/:id" element={<ProductDetail />} />
     </Routes>
   );
 }
