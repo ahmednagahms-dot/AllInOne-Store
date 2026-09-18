dfe6ecd3712da19763c2242051a026ef1c46f3b0
taha ahmed <tahahodhod650@gmail.com>
2026-09-18 08:50:44 +0300

    complete setup i18n laibrary

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,9 @@ import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 import { CartProvider } from "./context/CartContext";
 import { WishlistProvider } from "./context/WishlistContext";
-import App from "./App.jsx";
 import "./index.css";
+import './i18n/config.js'
+import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
