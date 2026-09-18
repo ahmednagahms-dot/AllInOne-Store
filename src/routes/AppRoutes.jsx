c1f7ed39ca5f755ccc1edb6e5cb118e7281cddce
rahma <rahmashoaib995@gmail.com>
2026-09-18 20:48:55 +0300

    feat: add login authentication and update schemas

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,9 +1,17 @@
 import { Routes, Route } from "react-router-dom";
+import Login from "../components/auth/Login";
+import Signup from "../components/auth/Signup";
+import ForgetPassword from "../components/auth/ForgetPassword";
 
 export default function AppRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<div >ALLINONE STORE</div>} />
+      <Route path="/" element={<Login />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/register" element={<Signup />} />
+      <Route path="/forgot" element={<ForgetPassword />} />
+      <Route path="/forgot-password" element={<ForgetPassword />} />
     </Routes>
   );
 }
