5548cd43b611176494df1625c9d85b5adf9cf727
taha ahmed <tahahodhod650@gmail.com>
2026-09-18 09:00:53 +0300

    create LanguageSwitcher button

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-
+import { useTranslation } from "react-i18next";
 import AppRoutes from "./routes/AppRoutes";
+import LanguageSwitcher from "./components/ui/LanguageSwitcher";
 
 export default function App() {
+  const { t } = useTranslation();
   return (
     <>
+    <LanguageSwitcher />
       <AppRoutes />
     </>
   );
---
+++ src/components/ui/LanguageSwitcher.jsx
@@ -0,0 +1,19 @@
+import { useTranslation } from 'react-i18next'
+const LanguageSwitcher = () => {
+    const {i18n } = useTranslation() 
+    const changeLanguage = (lang) => {
+        i18n.changeLanguage(lang);
+    }
+  return (
+    <div>
+        <button onClick={() => changeLanguage("en")}>
+            en
+        </button>
+        <button onClick={() => changeLanguage("ar")}>
+            ar
+        </button>
+    </div>
+  )
+}
+
+export default LanguageSwitcher
