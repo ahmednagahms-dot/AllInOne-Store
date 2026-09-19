9df4bb7f8e4a7047deae2809d4c38eded67243a1
ahmedsharaf7 <ahmed6datlst@gmail.com>
2026-09-19 10:14:52 +0300

    restore local changes

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,12 +1,15 @@
 import { Routes, Route } from "react-router-dom";
-import ShoppingCart from "../components/ui/Cart.jsx";
+import ShoppingCart from "../pages/Cart.jsx";
+import MainLayout from "../components/layout/MainLayout";
+import Home from "../pages/Home";
 
 export default function AppRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<div>ALLINONE STORE</div>} />
+        <Route element={<MainLayout />}>
+        <Route path="/" element={<Home />} />
       <Route path="/cart" element={<ShoppingCart />} />
-      <Route path="/shop" element={<div>Shop Page</div>} />
+      </Route>
     </Routes>
   );
 }
